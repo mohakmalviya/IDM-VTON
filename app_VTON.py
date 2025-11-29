56a54a1e013c3ee1d4b6366ecf8a79dc376020f5
senku <mohakmalviya2000@gmail.com>
2025-11-30 03:50:47 +0530

    updated app_VTON.py

--- app_VTON.py
+++ app_VTON.py
@@ -349,9 +349,22 @@ def start_tryon(dict, garm_img, garment_des, category, is_checked, is_checked_cr
 garm_list = os.listdir(os.path.join(example_path, "cloth"))
 garm_list_path = [os.path.join(example_path, "cloth", garm) for garm in garm_list]
 
+human_list = os.listdir(os.path.join(example_path, "human"))
+human_list_path = [os.path.join(example_path, "human", human) for human in human_list]
+
+human_ex_list = []
+for ex_human in human_list_path:
+    if "Jensen" in ex_human or "sam1 (1)" in ex_human:
+        ex_dict = {}
+        ex_dict['background'] = ex_human
+        ex_dict['layers'] = None
+        ex_dict['composite'] = None
+        human_ex_list.append(ex_dict)
 
 image_blocks = gr.Blocks().queue()
 with image_blocks as demo:
+    gr.Markdown("## V17 - IDM-VTON 👕👔👚 improved by SECourses : 1-Click Installers Latest Version On : https://www.patreon.com/posts/122718239")
+    gr.Markdown("Virtual Try-on with your image and garment image. Check out the [source codes](https://github.com/yisol/IDM-VTON) and the [model](https://huggingface.co/yisol/IDM-VTON)")
     with gr.Row():
         with gr.Column():
             imgs = gr.ImageEditor(
@@ -369,7 +382,11 @@ with image_blocks as demo:
                 is_checked = gr.Checkbox(label="Yes", info="Use auto-generated mask (Takes 5 seconds)", value=True)
             with gr.Row():
                 is_checked_crop = gr.Checkbox(label="Yes", info="Use auto-crop & resizing", value=True)
-
+            example = gr.Examples(
+                inputs=imgs,
+                examples_per_page=2,
+                examples=human_ex_list
+            )
         with gr.Column():
             garm_img = gr.Image(label="Garment", sources='upload', type="pil")
             with gr.Row(elem_id="prompt-container"):
